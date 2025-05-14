5b30f17a80f8c2f03130a63df5fbc923a08aa382
DesireJYA <124085984+Desire7777@users.noreply.github.com>
2025-05-14 15:50:37 +0000

    Update backupscript.sh

--- backupscript.sh
+++ backupscript.sh
@@ -13,14 +13,14 @@ DATE="$(date +%Y%m%d)"
 SCRIPT_PATH="$(realpath "$0")"
 CRON_JOB="0 3 * * * $SCRIPT_PATH"
 
-log.message(){
-        echo "[$DATE] $1" >> "LOG_FILE"
+log(){
+        echo "[$(date '+%Y-%m-%d')] $1" >> $LOG_FILE
         }
 
 if [ "$1" == "--install-cron" ]; then
-    if ! crontab -l 2>/dev/null | grep -F "$SCRIPT_PATH" >/dev/null; then
+    if ! crontab -l 2>/dev/null | grep -F $SCRIPT_PATH >/dev/null; then
         (crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
-        log.nessage "Cronjob Installed : $CRON_JOB"
+        log"Cronjob Installed : $CRON_JOB"
         echo "Cronjob installed : $CRON_JOB"
     else
     echo "Cronjob Exists"
@@ -31,21 +31,22 @@ if [ "$1" == "--install-cron" ]; then
 if [ ! -d "$BACKUP_DIR" ]; then
       mkdir -p "$BACKUP_DIR"
       log.message "$BACKUP_DIR created"
-      fi
-      exit 0
+            exit 0
+fi
 
-tar -czf "$BACKUP_DIR/backup_$DATE.tar.gz" "$SOURCE_DIR" 2>> "$LOG_FILE"
+
+tar -czf $BACKUP_DIR/backup_$DATE.tar.gz $SOURCE_DIR 2>> $LOG_FILE
 
 if [ $? -eq 0 ]; then
-      log.message "Backup Complete : backup_$DATE.tar.gz"
+      log "Backup Complete : backup_$DATE.tar.gz"
     else 
-    log.message "backup failed"
+    log "backup failed"
     exit 1
 fi
 
 #Clear 7 day old backups
-find "$BACKUP_DIR" -type f -name "*.tar.gz -mtime +7 -delete
-log.message "Old backups cleaned up"
+find "$BACKUP_DIR" -type f -name "*.tar.gz -mtime +2 -delete
+log "Old backups cleaned up"
 
 exit 0
 
