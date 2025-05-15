0877d6ef6ed4bedf14a26a13519da0ce458c95a2
DesireJYA <124085984+Desire7777@users.noreply.github.com>
2025-05-15 15:20:18 +0000

    Update userManagement.sh

--- userManagement.sh
+++ userManagement.sh
@@ -1,123 +1,47 @@
 #!/bin/hash
+source usermgfunctions.sh
 
-#filename = usermg.sh
+#file name = usermg.sh
+#function file = usermgfunctions.sh
 
 
-echo -e "Hello, what task would you like to perform today? (Enter 1-5) \n
+
+echo -e "Hello, what task would you like to perform today? (Enter 1-7) \n
 	1. Add a User \n
 	2. Delete a User \n
 	3. Reset User Password \n
 	4. Create a New group \n
 	5. Delete a Group \n
-	6. Reset Group Password"
+	6. Reset Group Password \n
+	7. Lock or Unlock Account"
 read Task
 
 case $Task in
-	1) echo "What name would you like to give to the new User?"
-		read Username
+	1) Add_User	
+	 	;;
 
-		if id $Username &>/dev/null; then
-		       echo "Username $Username already exists"
-          	else
-   			echo "$Username available"
-		
-			if [ $? -eq 0 ]; then
-                                sudo useradd $Username
-                        
-
-                                 if [ $? -eq 0 ]; then
-                                echo "User added"
-                                  fi
-
-                        fi
-
-		fi
-
-		
-	 ;;
-
-	2) echo "Which Username would you like to delete"
-		read Userdel
-          
-		if id $Userdel &>/dev/null; then
-			sudo userdel -r $Userdel
-		
-			
-			if [ $? -eq 0 ]; then
-				echo "User $Userdel deleted"
-			fi
-		else 
-			echo "User $Userdel not found"
-		fi
-		;;
-	3) echo "Which User Password would you like to reset"
-		read UserPswd
-		
-		if id $UserPswd &>/dev/null; then
-			sudo passwd $UserPswd
-			
-
-			if [ $? -eq 0 ]; then
-                                echo "$UserPswd Password changed "
-			fi
-
-		else 
-			echo "$UserPswd does not exist"
-		fi
-		;;
-	4) echo "Enter the Name of the Group you would like to create"
-		read GroupName
-
-	      if getent group $GroupName &>/dev/null; then
-                       echo "Group Name $GroupName already exists"
-                else
-                        echo "$GroupName available"
-                        
-                fi
-
-        
-                        if [ $? -eq 0 ]; then
-                                sudo groupadd $GroupName
-                                
-                        fi
-                
-                        if [ $? -eq 0 ]; then
-                                echo "Group added"
-                        fi
-			;;
-	5) echo "Which Group name would you like to delete"
-                read Groupdel
-
-                if getent group $Groupdel &>/dev/null; then
-                        sudo groupdel $Groupdel
-                       
-
-                        if [ $? -eq 0 ]; then
-                                echo "Group $Groupdel deleted"
-                        fi
-                else
-                        echo "$Groupdel not found"
-                fi
+	2) Del_User
 		;;
 
-	6) echo "Which Group Password would you like to reset"
-                read GroupPswd
-
-                if getent group $GroupPswd &>/dev/null; then
-                        sudo gpasswd $GroupPswd
-                       
-
-                        if [ $? -eq 0 ]; then
-                                echo "$GroupPswd Password changed "
-                        fi
-
-                else
-                        echo "$GroupPswd does not exist"
-                fi
+	3) User_Passwd
 		;;
+
+	4) Add_Group
+		;;
+
+	5) Del_Group
+		;;
+
+	6) Group_Passwd
+		;;
+	
+	7) Account_lock
+		;;
+
 	*) echo "Invalid Response"
 		;;
 esac
 
 
 
+exit 0
